96934cb295a7227af0acc17e2f85837b70aa446e
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-06-05 19:02:28 -0400

    build a small API, but found it to be slower

---
+++ Poly.m
@@ -0,0 +1,42 @@
+classdef Poly < handle
+    properties
+        c
+    end
+    
+    methods
+        function this = Poly(order, init)
+            this.c = zeros(1, order);
+            if nargin >= 2
+                this.c(1) = init;
+            end
+        end
+        
+        function v = calc(poly,t, order)
+            if order == 0
+                if t == 0
+                    v =  poly.c(1);
+                    return;
+                end
+                v = t;
+                v(:) = poly.c(end);
+                for i = size(poly.c, 2)-1 : -1 : 1
+                    v = v .* t;
+                    v = v + poly.c(i);
+                end
+            else
+                c2 = poly.c .* factorial(0:size(poly.c, 2)-1);
+                if t == 0
+                    v = c2(1+order);
+                    return;
+                end
+                v = t;
+                v(:) = c2(end);
+                for i = size(c2, 2)-1 : -1 : 1 + order
+                    v = v .* t / (i - order);
+                    v = v + c2(i);
+                end
+            end
+         end
+    end
+    
+end
