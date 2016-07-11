cc9bc78b448f49ea7411b82f61625d55a38463d0
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-07-11 11:47:00 -0400

    modify calc to use for loop

--- test_circuit.m
+++ test_circuit.m
@@ -3,15 +3,15 @@ problem = test_circuit_problem;
 s = simulator(problem);
 
 %% compute
-test_circuit_problem.intOrder(50);
-test_circuit_problem.segLen(1/70);
+test_circuit_problem.intOrder(70);
+test_circuit_problem.segLen(1/50);
 s.compute(5)
 
 %% plot taylor
 figure(2)
 hold off
-test_circuit_problem.whichVar(3);
-t = linspace(-test_circuit_problem.segLen()*2, test_circuit_problem.segLen()*2, 1000);
+test_circuit_problem.whichVar(1);
+t = linspace(-1, 5, 1000);
 c =  s.calc(t, 0);
 plot( t , s.calc(t) , '-');
 title('segment Length: 1/1000, integration order: 10');
