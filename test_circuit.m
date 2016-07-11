270203abe283a5e03b77a26480b36554fab40a1d
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-07-11 14:31:56 -0400

    generate more plots 7/11

--- test_circuit.m
+++ test_circuit.m
@@ -3,19 +3,24 @@ problem = test_circuit_problem;
 s = simulator(problem);
 
 %% compute
-test_circuit_problem.intOrder(5);
-test_circuit_problem.segLen(1/5000);
+test_circuit_problem.intOrder(30);
+test_circuit_problem.segLen(1/1000);
 s.compute(5)
 
 %% plot taylor
 figure(2)
 hold off
+t = linspace(-1, 5, 1000);
 test_circuit_problem.whichVar(1);
-t = linspace(0, 0.01, 1000);
-c =  s.calc(t, 0);
-vv = s.calc(t);
-plot( t , vv , '-');
-title('segment Length: 1/1000, integration order: 10');
+plot( t , s.calc(t) , 'b');
+hold on
+test_circuit_problem.whichVar(2);
+plot( t , s.calc(t) , 'b:');
+test_circuit_problem.whichVar(3);
+plot( t , s.calc(t) , 'b-.');
+legend('1', '2', '3')
+title('segment Length: 1/1000, integration order: 30, compare');
+print -djpeg order-30-len-1000-compare
 
 %%
 % figure(2)
