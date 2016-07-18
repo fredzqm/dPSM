b8746525a3b0c91d0218d80f067f1bab09ef0fda
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-07-17 22:39:31 -0400

    remove velocity plot

--- test_sync.m
+++ test_sync.m
@@ -6,16 +6,6 @@ s = simulator(problem);
 %% compute
 s.compute(50);
 
-%% plot taylor
-figure(1)
-hold on
-tt = 0 :0.01: 50;
-for i = 1: test_sync_problem.N
-    test_sync_problem.whichVar(i);
-    plot( tt , s.calc(tt, 1) , '-');
-end
-print -djpeg velocity
-
 %% plot position
 figure(2)
 hold on
