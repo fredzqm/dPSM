a470b5fae552ccf2228fe6acfb4e4462188b1531
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-07-17 22:41:06 -0400

    plot difference instead

--- test_sync.m
+++ test_sync.m
@@ -8,11 +8,15 @@ s.compute(50);
 
 %% plot position
 figure(2)
+hold off
+plot(1, 1)
 hold on
 tt = 0 :0.01: 50;
-var1 = s.calc(tt);
-for i = 2: test_sync_problem.N
+% test_sync_problem.whichVar(1);
+for i = 1: test_sync_problem.N
     test_sync_problem.whichVar(i);
-    plot( tt , s.calc(tt)-var1 , '-');
+    var{i} = s.calc(tt);
 end
-print -djpeg relativePos
+for i = 1: test_sync_problem.N-1
+    plot( tt , var{i}-var{i+1} , '-');
+end
