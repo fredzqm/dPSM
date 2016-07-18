0b82e3a515ba53976ca15cc177604c200f4c42ef
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-07-17 22:57:09 -0400

    acquire exactly the save plot as Dr. Isaia

--- test_sync2.m
+++ test_sync2.m
@@ -1,24 +1,33 @@
  %% initialize sqrt(x+1)*cos(x^2)
-% clear;
-problem = test_sync_problem;
-s = simulator(problem);
+clear;
 test_sync_problem.K(1.25);
 test_sync_problem.tau(2);
-test_sync_problem.intOrder(10);
+test_sync_problem.intOrder(20);
+% prepare the initial data --- T, U & V
+N = test_sync_problem.N;
+tau = test_sync_problem.tau();
+IV = 0.5-(0:11)*6/12; om = pi/2*ones(1,12);
+IV = IV + rand(1,12)*10^-1;
+T = zeros(N,1); U = zeros(N^2,1); V = zeros(N^2,1);             
+for j = 1:N
+    T(j,1:2) = [IV(1,j) om(j)];
+end
+for i = 1:N
+    ii = (i-1)*N;
+    for j = 1:N
+        t = calc(T(j,:),-1*tau, 0)-calc(T(i,1),0, 0);
+        U(j+ii,1) = sin( t );
+        V(j+ii,1) = cos( t );
+    end
+end
+init.T = T; init.U = U; init.V = V;
+% end prepare the initial data --- T, U & V
+problem = test_sync_problem(init);
+s = simulator(problem);
 
 
 %% compute
 s.compute(50);
-
-%% plot taylor
-figure(1)
-hold on
-tt = 0 :0.01: 50;
-for i = 1: test_sync_problem.N
-    test_sync_problem.whichVar(i);
-    plot( tt , s.calc(tt, 1) , '-');
-end
-print -djpeg velocity
 
 %% plot position
 figure(2)
