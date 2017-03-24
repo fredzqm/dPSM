29d5174fb2a8ee367e92f4b4e5b1f532fcf662c0
Fred Zhang <fredzqm@gmail.com>
2017-03-24 19:49:00 -0400

    refactor deriv

--- util/deriv.m
+++ util/deriv.m
@@ -1,5 +1,15 @@
-function mat = deriv(mat)
-    s = size(mat);
-    mat = mat(:, 2:end) * diag(1:s(2)-1);
-    mat = [mat zeros(s(1), 1)];
+function mat = deriv(mat, order)
+    if nargin < 2
+        order = 1;
+    end
+    mat = mat * derivMatrix(size(mat, 2), order);
+end
+
+function x = derivMatrix(n, order)
+    x = diag(ones(n-order, 1));
+    ls = ones(1, n-order);
+    for i = 1 : order;
+        ls = ls .* (i:i+n-order-1);
+    end
+    x = [[zeros(order, n-order); diag(ls)] zeros(n,order)];
 end
